b675086bd2689892bcc5d37e2282023fa99e2e2e
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-08-10 14:33:31 -0300

    svg adicionado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
     <div className="App">
         <p>
           Hello Tindev
         </p>
+        <img src={logo} alt="Tindev"/>
     </div>
   );
 }
