56104b2127552007127131f98416eaa1c00e79ce
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-08-10 14:56:47 -0300

    estilização do componente de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Login.css';
+
 import logo from '../assets/logo.svg';
 
 
 export default function Login() {
     return (
-        <img src={logo} alt="Tindev"/>
+        <div className="login-container">
+            <form>
+                <img src={logo} alt="Tindev"/>
+
+                <input
+                    placeholder="Digite seu usuario"
+                />
+
+                <button type="submit">Enviar</button>
+
+            </form>
+        </div>
     );
 }
