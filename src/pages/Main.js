bbfdb123269af1a9e27c5d8de93e2f1b77fb29d6
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-08-10 23:01:12 -0300

    mock

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
 
 export default function Main({ match }) {
+    const repeat = [1, 2, 3, 4];
     return (
-        <h1>{match.params.id}</h1>
+        <div className="main-container">
+            <img src={logo} alt="Tinder"/>
+
+            <ul>
+                {repeat.map(v => (
+                    <li key={v}>
+                        <img
+                            src="https://avatars0.githubusercontent.com/u/52754546?v=4" 
+                            alt="User img"/>
+                        
+                        <footer>
+                            <strong>Vitor Emidio</strong>
+                            <p>Programador TCE-RJ DTI</p>
+                        </footer>
+
+                        <div className="buttons">
+                            <button type="button">
+                                <img src={like} alt="Like"/>
+                            </button>
+                            <button type="button">
+                                <img src={dislike} alt="Dislike"/>
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
