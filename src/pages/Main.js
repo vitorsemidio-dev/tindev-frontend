4c66e6f61eb1459745896b02bfd77089298bd78d
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-08-10 23:26:53 -0300

    Renderizando usuarios disponiveis para dah (dis)likes

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
 
 import './Main.css';
+import api from '../services/api';
 
 export default function Main({ match }) {
-    const repeat = [1, 2, 3, 4];
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: { user: match.params.id }
+            })
+
+            // console.log(response.data);
+            setUsers(response.data);
+        }
+
+        loadUsers();
+    }, [match.params.id]);
+
     return (
         <div className="main-container">
             <img src={logo} alt="Tinder"/>
 
             <ul>
-                {repeat.map(v => (
-                    <li key={v}>
+                {users.map(user => (
+                    <li key={user._id}>
                         <img
-                            src="https://avatars0.githubusercontent.com/u/52754546?v=4" 
+                            src={user.avatar} 
                             alt="User img"/>
                         
                         <footer>
-                            <strong>Vitor Emidio</strong>
-                            <p>Programador TCE-RJ DTI</p>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
                         </footer>
 
                         <div className="buttons">
